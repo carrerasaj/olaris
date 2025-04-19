19e8114409ab80f138e29ce2cd29e5f1d08e3747
Olaris <alan@olaris.co.uk>
2025-04-19 06:53:01 +0100

    Add .htaccess for static file serving, simplify index.php, add fallbacks, ensure logo.png, debug 1.8

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-$file = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"];
-if (file_exists($file) && !is_dir($file)) {
-    return false; // Let Heroku serve the file
-}
-include_once("home.html");
-?>
+<?php include_once("home.html"); ?>
