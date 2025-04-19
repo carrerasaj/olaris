ec6e7a6b09bc6da9a08d8c8f89fd2284eaf5c479
Olaris <alan@olaris.co.uk>
2025-04-19 06:39:23 +0100

    Update home.html with delayed constellation script, add fallbacks, ensure video serving, debug 1.8

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php include_once("home.html"); ?>
+<?php
+$file = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"];
+if (file_exists($file) && !is_dir($file)) {
+    return false; // Let Heroku serve the file
+}
+include_once("home.html");
+?>
